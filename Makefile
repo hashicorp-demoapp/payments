3bfa692126de48cbb921b9f5b94c12013e8c2ba7
lanceplarsen <lance@hashicorp.com>
2021-01-05 20:17:12 -0700

    fix logger

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ clean:
 
 run:
 	./gradlew build
-	java -jar build/libs/spring-boot-payments-0.0.7.jar
+	java -jar build/libs/spring-boot-payments-0.0.8.jar
 
 build_docker:
 	docker build -t hashicorpdemoapp/payments:latest .
--- src/main/java/payments/ApplicationConfig.java
+++ src/main/java/payments/ApplicationConfig.java
@@ -1,8 +1,10 @@
 package payments;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
+@Configuration
 public class ApplicationConfig {
 
 	@Bean
